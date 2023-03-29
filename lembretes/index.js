ac82d2a812d0148dd6349d0ec180cfad3fbcb723
Anderson Sanches <andycds@gmail.com>
2023-03-28 23:17:55 -0300

    Fundação do projeto

---
+++ lembretes/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
+app.use(bodyParser.json());
+
+lembretes = {};
+contador = 0;
+
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes);
+});
+
+app.put('/lembretes', async (req, res) => {
+    contador++;
+    const { texto } = req.body;
+    lembretes[contador] = {
+        contador, texto
+    }
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto
+        },
+    });
+    res.status(201).send(lembretes[contador]);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: "ok"});
+});
+
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000');
+});
