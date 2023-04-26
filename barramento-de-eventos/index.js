7e49126ff919d0d07a64f93aaec20a58e185ad8e
Anderson Sanches <andycds@gmail.com>
2023-04-26 00:15:19 -0300

    Microsserviço de consulta se recupera após falha

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -4,19 +4,29 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
+const eventos = [];
 
-app.post('/eventos', (req, res) => {
+app.post('/eventos', async (req, res) => {
     const evento = req.body;
-    //envia o evento para o microsserviço de lembretes
-    axios.post('http://localhost:4000/eventos', evento);
-    //envia o evento para o microsserviço de observações
-    axios.post('http://localhost:5000/eventos', evento);
-    //envia o evento para o microsserviço de consulta
-    axios.post('http://localhost:6000/eventos', evento);
-    //envia o evento para o microsserviço de classificação
-    axios.post('http://localhost:7000/eventos', evento);
+    eventos.push(evento);
+    try {
+        //envia o evento para o microsserviço de lembretes
+        await axios.post('http://localhost:4000/eventos', evento);
+        //envia o evento para o microsserviço de observações
+        await axios.post('http://localhost:5000/eventos', evento);
+        //envia o evento para o microsserviço de consulta
+        await axios.post('http://localhost:6000/eventos', evento);
+        //envia o evento para o microsserviço de classificação
+        await axios.post('http://localhost:7000/eventos', evento);
+    } catch (error) {
+        console.log("Algum serviço está morto.")
+    }
     res.status(200).send({ msg: "ok" });
 });
+
+app.get('/eventos', (req, res) => {
+    res.send(eventos);
+})
 
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000.')
--- consulta/index.js
+++ consulta/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const axios = require("axios");
 
 baseConsulta = {};
 
@@ -35,4 +36,12 @@ app.post("/eventos", (req, res) => {
     res.status(200).send(baseConsulta);
 });
 
-app.listen(6000, () => console.log("Consultas. Porta 6000"));
+app.listen(6000, async () => {
+    console.log("Consultas. Porta 6000");
+    const resp = await axios.get('http://localhost:10000/eventos');
+    resp.data.forEach((valor, indice, colecao) => {
+        try {
+            funcoes[valor.tipo](valor.dados);
+        } catch (err) {}
+    });
+});
